f4693194d26512df8980bf2bf9729e4141de976f
Walker Negrao <1882452@tcs.com>
2025-06-10 14:40:25 -0300

    Atualização

--- secao-06/src/main/java/br/com/walker/aprendizado_sobre_springboot_erudio/controller/PersonController.java
+++ secao-06/src/main/java/br/com/walker/aprendizado_sobre_springboot_erudio/controller/PersonController.java
@@ -4,10 +4,9 @@ import br.com.walker.aprendizado_sobre_springboot_erudio.model.Person;
 import br.com.walker.aprendizado_sobre_springboot_erudio.service.PersonService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/person")
@@ -15,8 +14,29 @@ public class PersonController {
     @Autowired
     private PersonService personService;
 
+    @GetMapping
+    public List<Person> findAll() {
+        return personService.findAll();
+    }
+
     @GetMapping(value = "/{id}", produces = MediaType.APPLICATION_JSON_VALUE)
     public Person findById(@PathVariable("id") String id) {
         return personService.findById(id);
     }
+
+    @PostMapping
+    public Person create(@RequestBody Person person) {
+        return personService.create(person);
+    }
+
+    @PutMapping
+    public Person update(@RequestBody Person person) {
+        return personService.update(person);
+    }
+
+    @DeleteMapping("/{id}")
+    public void delete(@PathVariable("id") String id) {
+        personService.delete(id);
+    }
+
 }
--- secao-06/src/main/java/br/com/walker/aprendizado_sobre_springboot_erudio/service/PersonService.java
+++ secao-06/src/main/java/br/com/walker/aprendizado_sobre_springboot_erudio/service/PersonService.java
@@ -3,6 +3,8 @@ package br.com.walker.aprendizado_sobre_springboot_erudio.service;
 import br.com.walker.aprendizado_sobre_springboot_erudio.model.Person;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.concurrent.atomic.AtomicLong;
 import java.util.logging.Logger;
 
@@ -11,6 +13,16 @@ public class PersonService {
     private final AtomicLong counter = new AtomicLong();
 
     private Logger logger = Logger.getLogger(PersonService.class.getName());
+
+    public List<Person> findAll() {
+        logger.info("Finding all People!");
+        List<Person> personList = new ArrayList<>();
+        for (int i = 0; i < 8; i++) {
+            Person person = mockPerson(i);
+            personList.add(person);
+        }
+        return personList;
+    }
 
     public Person findById(String id) {
         logger.info("Finding one Person!");
@@ -21,6 +33,32 @@ public class PersonService {
         person.setLastName("Negrão");
         person.setAddress("Londrina - Paraná - Brasil");
         person.setGender("Male");
+        return person;
+    }
+
+    public Person create(Person person) {
+        logger.info("Creating one Person!");
+        return person;
+    }
+
+    public Person update(Person person) {
+        logger.info("Updating one Person!");
+        return person;
+    }
+
+    public void delete(String id) {
+        logger.info("Deleting one Person!");
+    }
+
+
+
+    private Person mockPerson(int i) {
+        Person person = new Person();
+        person.setId(counter.incrementAndGet());
+        person.setFirstName("Firstname " + i);
+        person.setLastName("Lastname " + i);
+        person.setAddress("Some Address in Brazil");
+        person.setGender("Male | Female");
         return person;
     }
 }
